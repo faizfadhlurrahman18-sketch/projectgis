92a22f1721bb9818a3eca1732f4a6a31ae62aab4
faizfadhlurrahman18-sketch <faizfadhlurrahman18@gmail.com>
2025-11-19 09:11:13 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const port = 3000;
 // Melayani semua file statis di folder project
 app.use(express.static(__dirname));
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
